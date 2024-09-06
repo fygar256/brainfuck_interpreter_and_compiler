c64e22635857c5ae1ba7bbdb44006d4c1aa48f5e
Taisuke Maekawa <fygar256@gmail.com>
2024-09-07 01:22:10 +0900

    Update bf.c

--- bf.c
+++ bf.c
@@ -1,69 +1,31 @@
 #include    <stdio.h>
 #include    <stdlib.h>
-#include    <malloc.h>
-#include    "fsize.h"
 void main(int argc,char *argv[]) {
     FILE *fp;
-    char array[30000]={0},*ptr=array,*pp;
-    int  loopstack[1000];
-    int  lsp=0,c,idx;
-    long len=0;
-
-    if (argc!=2) {
-        fprintf(stderr,"Usage %s <file>\n",argv[0]);
-        exit(1);
-    }
+    char prog[65536]={0},array[30000]={0},*ptr=array;
+    int  loopstack[1000],lsp=0,idx,len;
 
     fp=fopen(argv[1],"rt");
-    len=fsize(fp);
-    pp=malloc(len);
-    idx=0;
-
-    while((c=fgetc(fp))!=EOF)
-            pp[idx++]=c;
+    for(len=0; (prog[len]=fgetc(fp))!=EOF;len++);
     fclose(fp);
 
-    idx=0;
-    while(idx<len) {
-        switch (c=pp[idx++]) {
-            case '>':
-                ptr++;
-                break;
-            case '<':
-                ptr--;
-                break;
-            case '+':
-                (*ptr)++;
-                break;
-            case '-':
-                (*ptr)--;
-                break;
-            case '.':
-                putchar(*ptr);
-                break;
-            case ',':
-                *ptr=getchar();
-                break;
+    for(idx=0;idx<len;idx++)
+        switch (prog[idx]) {
+            case '>': ptr++; break;
+            case '<': ptr--; break;
+            case '+': (*ptr)++; break;
+            case '-': (*ptr)--; break;
+            case '.': putchar(*ptr); break;
+            case ',': *ptr=getchar(); break;
             case '[':
-                idx--;
                 if (*ptr)
                     loopstack[lsp++]=idx++;
-                else {
-                    int nest=0;
-                    while(idx<len)
-                        if ((c=pp[idx++])=='[')
-                            nest+=1;
-                        else if (c==']')
-                            if (--nest==0)
-                                break;
-                }
+                else for(int nest=0;idx<len;)
+                        if (prog[idx++]=='[') nest++;
+                        else if (prog[idx-1]==']')
+                            if (!--nest) break;
                 break;
-            case ']':
-                idx=loopstack[--lsp];
-                break;
+            case ']': idx=loopstack[--lsp]; break;
             default:
             }
-        }
-    free(pp);
 }
-
