df3e53ab721d3ddd4f4a14cdf7aedd2833c067c2
Taisuke Maekawa <fygar256@outlook.jp>
2025-07-06 18:37:03 +0900

    Add files via upload

---
+++ bfsf.py
@@ -0,0 +1,64 @@
+#!/usr/local/bin/python3
+import sys
+
+def lsout(loopstack):
+    return str(loopstack).replace(', ','_').replace('[','').replace(']','')
+
+def main():
+    if len(sys.argv)!=2:
+        print(f"Usage: {sys.argv[0]} <file>")
+        return
+    print("    .section .text")
+    print("    .globl _start")
+    print("_start:")
+    print("    movabs $_my_data,%rsi")
+
+    f=open(sys.argv[1],"rt")
+    loopstack=[]
+    lf='['
+    for s in f:
+        for c in s:
+            if c=='>':
+                print("    inc     %rsi")
+            elif c=='<':
+                print("    dec     %rsi")
+            elif c=='+':
+                print("    incb     (%rsi)")
+            elif c=='-':
+                print("    decb     (%rsi)")
+            elif c=='.':
+                print("    mov     $4,%eax") # syscall Write
+                print("    mov     $1,%rdi") # fd: stdout
+                print("    mov     $1,%rdx") # count
+                print("    syscall")
+            elif c==',':
+                print("    mov     $3,%rax") # syscall Read
+                print("    mov     $0,%rdi") # fd: stdin
+                print("    mov     $1,%rdx") # count
+                print("    syscall")
+            elif c=='[':
+                if lf==']':
+                    loopstack[len(loopstack)-1]+=1
+                else:
+                    loopstack.append(1)
+                lf='['
+                print(f"    jmp     LE{lsout(loopstack)}")
+                print(f"LB{lsout(loopstack)}:")
+            elif c==']':
+                if lf==']':
+                    loopstack.pop()
+                lf=']'
+                print(f"LE{lsout(loopstack)}:")
+                print("    cmp     %dh,(%rsi)")
+                print(f"    jne     LB{lsout(loopstack)}")
+    f.close()
+    print("    mov     $1,%rax")  # syscall exit
+    print("    xor     %rdi,%rdi") # status = 0
+    print("    syscall")
+    print(".section .bss")
+    print("_my_data: .zero 65536")
+
+if __name__=='__main__':
+    main()
+    exit(0)
+
